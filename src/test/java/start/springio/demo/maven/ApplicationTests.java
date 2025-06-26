250009e0e00be535523cbe4e0aa5094c2cb767ef
Loumia94 <loumia.mbk@gmail.com>
2025-06-26 16:06:10 +0200

    Initial commit – projet Spring Boot Maven

---
+++ src/test/java/start/springio/demo/maven/ApplicationTests.java
@@ -0,0 +1,13 @@
+package start.springio.demo.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
